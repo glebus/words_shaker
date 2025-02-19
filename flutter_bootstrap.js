862e95fb62366be6100f98d0972b46c3c3d6c931
glebus <glebus@users.noreply.github.com>
2025-02-19 13:37:21 +0000

    deploy: 03617b0b356d6a247d58f5407f0a8b7b4ca0f4f7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "744715309"
+    serviceWorkerVersion: "289280856"
   }
 });
